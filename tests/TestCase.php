00f999ae7a69f1f9a35d5be462e7be8950c37037
VictorPennington <victorpennington@gmail.com>
2026-07-28 13:02:33 +0000

    Adding test env. to config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        static $printed = false;
+        if (! $printed) {
+            $printed = true;
+            $defaultConnection = config('database.default');
+            $databaseName = config("database.connections.{$defaultConnection}.database");
+
+            fwrite(STDERR, "\n[TEST ENV] app_env=" . app()->environment() . " default_db=" . $defaultConnection . " database=" . $databaseName . "\n");
+        }
+    }
 }
